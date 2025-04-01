19b183def95836eab65e2d08e9e8a20fb2bebb7f
Pingolu <7nb@hotmail.fr>
2025-04-01 09:49:01 +0200

    fixtures

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -5,39 +5,56 @@ use PHPUnit\Framework\TestCase;
 
 class QueueTest extends TestCase
 {
+
+    protected $queue;
+
+    //fixture
     protected function setUp(): void
     {
-
+        $this->queue = new Queue;
     }
 
+    //mainly used when creating external resources
+    protected function tearDown(): void
+    {
+        unset($this->queue);
+    }
 
     // test depended on = producer
-    public function testNewQueueIsEmpty(): Queue
+    public function testNewQueueIsEmpty()
     {
-        $queue = new Queue;
 
-        $this->assertSame(0, $queue->getCount());
+        $this->assertSame(0, $this->queue->getCount());
 
-        return $queue;
     }
 
     // dependend test = consumer
-    #[Depends('testNewQueueIsEmpty')]
-    public function testAnItemIsAddedToTheQueue(Queue $queue): Queue
+    // #[Depends('testNewQueueIsEmpty')]
+    public function testAnItemIsAddedToTheQueue()
     {
-        $queue->push('hello');
+        $this->queue->push('hello');
 
-        $this->assertSame(1, $queue->getCount());
+        $this->assertSame(1, $this->queue->getCount());
 
-        return $queue;
     }
 
-    #[Depends('testAnItemIsAddedToTheQueue')]
-    public function testAnItemIsRemovedFromTheQueue(Queue $queue): void
-    {
-        $item = $queue->pop();
+    //
 
-        $this->assertSame(0, $queue->getCount());
-        $this->assertSame('hello', $item);
+    public function testAnItemIsRemovedFromTheQueue(): void
+    {
+        $this->queue->push('test');
+        $item = $this->queue->pop();
+
+        $this->assertSame(0, $this->queue->getCount());
+        $this->assertSame('test', $item);
     }
+
+
+    public function testAnItemIsRemovedFromTheFrontOfTheQueue()
+    {
+
+    }
+
+
+
 }
